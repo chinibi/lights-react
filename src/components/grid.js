f8ad8d6673a30d5df1da504c5c2135e7e693c519
Trevor Pham <trevor.dt.pham@gmail.com>
2016-09-07 12:50:56 -0700

    5x5 grid and clicking also switches adjacent tiles

--- src/components/grid.js
+++ src/components/grid.js
@@ -7,15 +7,22 @@ export default class Grid extends Component {
     const initialState = 'blue';
 
     this.state = {colors: [
-      ['blue', 'blue', 'blue'],
-      ['blue', 'blue', 'blue'],
-      ['blue', 'blue', 'blue']
+      ['blue', 'blue', 'blue', 'blue', 'blue'],
+      ['blue', 'blue', 'blue', 'blue', 'blue'],
+      ['blue', 'blue', 'blue', 'blue', 'blue'],
+      ['blue', 'blue', 'blue', 'blue', 'blue'],
+      ['blue', 'blue', 'blue', 'blue', 'blue']
     ]};
   }
 
   onSquareClick(i, j) {
     let newColors = this.state.colors;
     newColors[i][j] = newColors[i][j] == 'blue' ? 'red' : 'blue';
+    if (i !== 0)                      newColors[i-1][j] = newColors[i-1][j] == 'blue' ? 'red' : 'blue';
+    if (i !== newColors[i].length-1)  newColors[i+1][j] = newColors[i+1][j] == 'blue' ? 'red' : 'blue';
+    if (j !== 0)                      newColors[i][j-1] = newColors[i][j-1] == 'blue' ? 'red' : 'blue';
+    if (j !== newColors.length-1)     newColors[i][j+1] = newColors[i][j+1] == 'blue' ? 'red' : 'blue';
+    
     this.setState({colors: newColors});
   }
 
