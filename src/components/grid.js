dfbe9b789884620a51e18fbe8fcb5b0f2a3c11ef
Trevor Pham <trevor.dt.pham@gmail.com>
2016-09-07 12:06:45 -0700

    remove comments

--- src/components/grid.js
+++ src/components/grid.js
@@ -40,26 +40,4 @@ export default class Grid extends Component {
       </div>
     );
   }
-
-  // render() {
-  //   return (
-  //     <div id="grid">
-  //       <div className="row">
-  //         <GridSquare key="0" onClick={this.onSquareClick.bind(this, 0)} color={this.state.colors[0]} />
-  //         <GridSquare key="1" onClick={this.onSquareClick.bind(this, 1)} color={this.state.colors[1]} />
-  //         <GridSquare key="2" onClick={this.onSquareClick.bind(this, 2)} color={this.state.colors[2]}/>
-  //       </div>
-  //       <div className="row">
-  //         <GridSquare key="3" onClick={this.onSquareClick.bind(this, 3)} color={this.state.colors[3]} />
-  //         <GridSquare key="4" onClick={this.onSquareClick.bind(this, 4)} color={this.state.colors[4]} />
-  //         <GridSquare key="5" onClick={this.onSquareClick.bind(this, 5)} color={this.state.colors[5]} />
-  //       </div>
-  //       <div className="row">
-  //         <GridSquare key="6" onClick={this.onSquareClick.bind(this, 6)} color={this.state.colors[6]} />
-  //         <GridSquare key="7" onClick={this.onSquareClick.bind(this, 7)} color={this.state.colors[7]} />
-  //         <GridSquare key="8" onClick={this.onSquareClick.bind(this, 8)} color={this.state.colors[8]} />
-  //       </div>
-  //     </div>
-  //   );
-  // }
 }
